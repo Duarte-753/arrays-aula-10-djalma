2dc5c8359b26f8ebae1549778947ffd5dd73bb28
curso.ads <curso.ads@LAB06-06>
2022-05-25 22:24:04 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_aula_10_djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] valores = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(String.Format("valor {0}", i));
+                valores[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(String.Format("Elemento {0} = valor {1}", i, valores[i]));
+            }
+
+            // alteração array
+
+            int posicao;
+
+            Console.WriteLine("Qual posição:");
+            posicao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(String.Format("Novo Valor da posição {0}:", posicao));
+            valores[posicao] = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(String.Format("Elemento {0} = Valor {1}",i, valores[i]));
+            }    
+        }
+    }
+}
